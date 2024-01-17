fa5326ed5e8df4ef3da1d63b4edf981199cc84dc
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-17 16:57:45 +0530

    Send data to server side using fetch and POST method

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,38 @@ function App() {
     .then(data =>setUsers(data));
   },[])
 
+  const hanldeAddUser = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    // create an object of user 
+    const user = {name,email};
+    console.log(user);
+    // form.reset();
+    fetch('http://localhost:5050/users',{
+      method: 'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data =>{
+      console.log(data);
+    })
+  }
   return (
     <>
       <h1>Display Users data</h1>
+      <div>
+        <h1>Add User</h1>
+        <form onSubmit={hanldeAddUser}>
+          <input name='name' type="text" placeholder='Enter your name' /><br /><br />
+          <input name='email' type="email" placeholder='Enter your email' /> <br />
+          <input type="submit" value="Add User" />
+        </form>
+      </div>
       <p>No of Users: {users.length}</p>
       <div>
         {
