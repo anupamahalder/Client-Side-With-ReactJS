43defb3eb34973b567a2b8de126449c8716c8263
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-17 10:21:17 +0530

    Load users data using fetch and the url of users data

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  // load data here we give empty array to 2nd parameter as we want to load data once
+  useEffect(()=>{
+    fetch('http://localhost:5050/users')
+    .then(res=>res.json())
+    .then(data =>setUsers(data));
+  },[])
+
+  return (
+    <>
+      <h1>Display Users data</h1>
+      <p>No of Users: {users.length}</p>
+    </>
+  )
+}
+
+export default App
